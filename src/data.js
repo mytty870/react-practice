6a99da38533479bdb474ae4f30f68bdeb458ecda
github mytty870 <otani11ace@icloud.com>
2021-10-26 23:08:29 +0900

    add category

--- src/data.js
+++ src/data.js
@@ -32,12 +32,12 @@ export const categories = [
   },
   {
     id: 2,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg",
     title: "LOUNGEWEAR STYLE"
   },
   {
     id: 3,
-    img: "https://images.pexels.com/photos/5886041/pexels-photo-5886041.jpeg",
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg",
     title: "AHAHA STYLE"
   },
 ]
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { Announcement } from "../components/Announcement"
+import { Categories } from "../components/Categories"
 import Navbar from "../components/Navbar"
 import { Slider } from "../components/Slider"
 
@@ -11,6 +12,7 @@ const Home = () => {
       <Announcement />
       <Navbar />
       <Slider />
+      <Categories />
     </div>
   )
 }
