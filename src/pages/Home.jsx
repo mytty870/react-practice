821ad0ea348144c3a3d1f26806a1c9bbc64618ae
github mytty870 <otani11ace@icloud.com>
2021-10-27 10:44:22 +0900

    add products

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,7 @@
 import { Announcement } from "../components/Announcement"
 import { Categories } from "../components/Categories"
 import Navbar from "../components/Navbar"
+import { Products } from "../components/Products"
 import { Slider } from "../components/Slider"
 
 
@@ -13,6 +14,7 @@ const Home = () => {
       <Navbar />
       <Slider />
       <Categories />
+      <Products />
     </div>
   )
 }
